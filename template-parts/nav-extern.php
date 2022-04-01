55e1f40739a3c19091b510b4f6fc3ce391fcf799
tBrause <brause.torsten@gmail.com>
2022-04-01 22:43:40 +0200

    class change

--- template-parts/nav-extern.php
+++ template-parts/nav-extern.php
@@ -11,7 +11,7 @@
 <?php
 $args = array(
     'theme_location' => 'extern_nav',
-    'items_wrap'  => '<nav class="sub-main-extern"><ul class="menu-theme-sites">%3$s</ul></nav>',
+    'items_wrap'  => '<nav class="sub-main-extern"><ul class="theme-sites-nav">%3$s</ul></nav>',
     'link_before' => '<i class="bloginfo-icon fab fa-dev"></i>',
     'fallback_cb' => '',
 );
