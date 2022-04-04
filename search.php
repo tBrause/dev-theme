9345f0d237cd557d9394a36733b5e728fbfe2993
tBrause <brause.torsten@gmail.com>
2022-04-05 01:20:37 +0200

    add header and footer

--- search.php
+++ search.php
@@ -1,29 +1,39 @@
-<?php
-global $query_string;
-$query_args = explode("&", $query_string);
-$search_query = array();
+<?php get_header(); ?>
 
-foreach ($query_args as $key => $string) {
-    $query_split = explode("=", $string);
-    $search_query[$query_split[0]] = urldecode($query_split[1]);
-} // foreach
+<main class="page-main">
+    <div class="wraper">
 
-$the_query = new WP_Query($search_query);
-if ($the_query->have_posts()) :
-?>
-    <!-- the loop -->
+        <?php
+        global $query_string;
+        $query_args = explode("&", $query_string);
+        $search_query = array();
 
-    <ul>
-        <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-            <li>
-                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            </li>
-        <?php endwhile; ?>
-    </ul>
-    <!-- end of the loop -->
+        foreach ($query_args as $key => $string) {
+            $query_split = explode("=", $string);
+            $search_query[$query_split[0]] = urldecode($query_split[1]);
+        } // foreach
 
-    <?php wp_reset_postdata(); ?>
+        $the_query = new WP_Query($search_query);
+        if ($the_query->have_posts()) :
+        ?>
+            <!-- the loop -->
 
-<?php else : ?>
-    <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-<?php endif; ?>
+            <ul>
+                <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                    <li>
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </li>
+                <?php endwhile; ?>
+            </ul>
+            <!-- end of the loop -->
+
+            <?php wp_reset_postdata(); ?>
+
+        <?php else : ?>
+            <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+        <?php endif; ?>
+
+    </div>
+</main>
+
+<?php get_footer(); ?>
